78fc3984bacda504d2fb857606f0817707c18034
Olarn U <olarn.u@gmail.com>
2017-02-02 14:00:25 +0700

    remove comment.

--- index.js
+++ index.js
@@ -2,13 +2,11 @@ var express = require('express');
 var ipGetter = require('./getIP');
 var fs = require('fs');
 var uuidV4 = require('uuid/v4');
-// var uuid = require('./getUUID');
 var PORT = 3000;
 	
 var app = express();
 app.get('/', function (req, res) {
 	  var ip = ipGetter.getServerIp();
-	  // var id = uuid.getUuid();
 	  const path = './uuid.txt';
 	  fs.readFile(path, 'utf8', function (err, data) {
 		  if (data == undefined) {
