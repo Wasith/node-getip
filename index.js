2f426388aab6f1f4f11bc4f51c2b0b715b040ff6
Olarn U <olarn.u@gmail.com>
2017-02-02 13:58:32 +0700

    Add UUID to make each server unique.

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 var express = require('express');
 var ipGetter = require('./getIP');
+var fs = require('fs');
+var uuidV4 = require('uuid/v4');
+// var uuid = require('./getUUID');
 var PORT = 3000;
 	
 var app = express();
 app.get('/', function (req, res) {
 	  var ip = ipGetter.getServerIp();
-	  res.send('Hello, world! My IP = ' + ip + "\n");
+	  // var id = uuid.getUuid();
+	  const path = './uuid.txt';
+	  fs.readFile(path, 'utf8', function (err, data) {
+		  if (data == undefined) {
+			  const id = uuidV4();
+			  fs.writeFile (path, id, function(err) {
+ 			  	if (err) throw err;
+			   	res.send('Hello, world! \nMy IP = ' + ip + "\nUUID = " + id + "\n");
+			  });
+		  } else {
+			  res.send('Hello, world! \nMy IP = ' + ip + "\nUUID = " + data + "\n");
+		  }
+        return data;
+      });
 });
 	
 app.listen(PORT);
